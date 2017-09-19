97845b94c4a907c61b175c613f4633160c971248
Yves Beutler <yves.beutler@hotmail.com>
2017-09-19 13:36:11 +0200

    create simple maven project

--- README.md
+++
@@ -1,2 +0,0 @@
-# bti7535-data-science
-This repository holds all my code examples dealing with today's difficulties of text analytics
---
+++ src/main/java/ch/yvesbeutler/excercise/tfd/Launcher.java
@@ -0,0 +1,11 @@
+package ch.yvesbeutler.excercise.tfd;
+
+/**
+ * @author yvesbeutler
+ * Term Frequency Distribution (TFD)
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Hello Data Science!");
+    }
+}
